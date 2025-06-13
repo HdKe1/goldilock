80bcd1533ccb24ce30bc59d23e178371ef108100
Aditya Poduval <adipoduval5@gmail.com>
2025-06-13 14:14:08 +0530

    changed db to postgres and connected

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Stats(models.Model):
+    username = models.CharField(max_length=100)
+    avg_wpm = models.FloatField(default = 0.0,null = True,blank = True)
+    last_wpm = models.FloatField(default = 0.0,null = True,blank = True)
